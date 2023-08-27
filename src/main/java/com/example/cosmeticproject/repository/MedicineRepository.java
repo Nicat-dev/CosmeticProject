e9f562832f924ed4c0e3ad2834130f3794f0ac54
Nicat-dev <nijatmehdiyevv@gmail.com>
2023-08-28 00:08:07 +0400

    medicine information

---
+++ src/main/java/com/example/cosmeticproject/repository/MedicineRepository.java
@@ -0,0 +1,9 @@
+package com.example.cosmeticproject.repository;
+
+import com.example.cosmeticproject.entity.Medicine;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MedicineRepository extends JpaRepository<Medicine,Long> {
+}
