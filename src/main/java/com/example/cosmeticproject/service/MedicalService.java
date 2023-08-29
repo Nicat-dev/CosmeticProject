4483c740deb956854b624e3164f27019dc708cc3
Nicat-dev <nijatmehdiyevv@gmail.com>
2023-08-29 15:18:42 +0400

    service

--- src/main/java/com/example/cosmeticproject/service/MedicalService.java
+++ src/main/java/com/example/cosmeticproject/service/MedicalService.java
@@ -1,4 +1,9 @@
 package com.example.cosmeticproject.service;
 
+import com.example.cosmeticproject.dto.request.MedicineRequest;
+import com.example.cosmeticproject.entity.Medicine;
+
 public interface MedicalService {
+
+    Medicine addMedicine(MedicineRequest request);
 }
