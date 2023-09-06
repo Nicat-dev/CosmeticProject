78728ac9737ffd7e1aa285d1a9c54d0228e06697
Nicat-dev <nijatmehdiyevv@gmail.com>
2023-09-06 11:56:03 +0400

    add DoctorMapper and update Service

--- src/main/java/com/example/cosmeticproject/controller/DoctorController.java
+++ src/main/java/com/example/cosmeticproject/controller/DoctorController.java
@@ -1,4 +1,28 @@
 package com.example.cosmeticproject.controller;
 
+import com.example.cosmeticproject.dto.DoctorDto;
+import com.example.cosmeticproject.dto.request.DoctorRequest;
+import com.example.cosmeticproject.service.DoctorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/doctor")
+@RequiredArgsConstructor
 public class DoctorController {
+
+    final DoctorService doctorService;
+
+    @GetMapping("/getAll")
+    public List<DoctorDto> getAllDoctor(){
+        return doctorService.getDoctors();
+    }
+
+    @PostMapping("/addDoctor")
+    public void addDoctor(@RequestBody DoctorRequest doctorRequest){
+        doctorService.saveDoctor(doctorRequest);
+    }
+
 }
