4b4b401c3bcbc885811324f5a3a3e95d9b990a01
Nicat-dev <nijatmehdiyevv@gmail.com>
2023-10-15 21:19:50 +0200

    Updated

--- src/main/java/com/example/cosmeticproject/controller/CustomerController.java
+++ src/main/java/com/example/cosmeticproject/controller/CustomerController.java
@@ -1,8 +1,10 @@
 package com.example.cosmeticproject.controller;
 
 import com.example.cosmeticproject.dto.CustomerDto;
+import com.example.cosmeticproject.dto.response.BaseResponse;
 import com.example.cosmeticproject.service.CustomerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +17,10 @@ public class CustomerController {
     final CustomerService customerService;
 
     @RequestMapping("/getCustomerById/{id}")
-    public CustomerDto getCustomerById(@PathVariable Long id){
-        return customerService.getCustomerById(id);
+    public ResponseEntity<BaseResponse<CustomerDto>> getCustomerById(@PathVariable Long id){
+        return ResponseEntity.ok(new BaseResponse<>(Boolean.TRUE,
+                "Customer successffully find",
+                customerService.getCustomerById(id)));
     }
 
     }
