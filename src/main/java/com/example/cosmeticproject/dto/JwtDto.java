ebd3e5c0ae40c02ffc2aa40c6c776c0157ebf90b
Nicat-dev <nijatmehdiyevv@gmail.com>
2023-11-08 23:23:12 +0100

    bugFix and create LoginRequest and JwtDto

---
+++ src/main/java/com/example/cosmeticproject/dto/JwtDto.java
@@ -0,0 +1,10 @@
+package com.example.cosmeticproject.dto;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class JwtDto {
+    String token;
+}
