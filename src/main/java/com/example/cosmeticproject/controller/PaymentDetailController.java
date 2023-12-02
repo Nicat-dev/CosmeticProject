f76ff4efd4c64a6dc2f8135b5cf0e71f6f3c8afe
Nicat-dev <nijatmehdiyevv@gmail.com>
2023-12-02 18:46:02 +0100

    write getById paymentController

--- src/main/java/com/example/cosmeticproject/controller/PaymentDetailController.java
+++ src/main/java/com/example/cosmeticproject/controller/PaymentDetailController.java
@@ -1,4 +1,26 @@
 package com.example.cosmeticproject.controller;
 
+import com.example.cosmeticproject.dto.PaymentDetailDto;
+import com.example.cosmeticproject.service.PaymentDetailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1/api/paymentDetail")
+@RequiredArgsConstructor
 public class PaymentDetailController {
+
+    private final PaymentDetailService service;
+
+
+    @GetMapping("/getById/{id}")
+    public ResponseEntity<PaymentDetailDto> paymentInfo(@PathVariable Long id){
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+
 }
